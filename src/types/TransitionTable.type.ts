fc7f60f9754677083aaf962ac387812961e910df
Elkin <ersantis@uninorte.edu.co>
2024-09-29 16:00:22 -0500

    Refactor TransitionTable type definition and add TransitionTableProps interface

---
+++ src/types/TransitionTable.type.ts
@@ -0,0 +1,13 @@
+import { Automaton } from "../utils/Automaton";
+
+export interface TransitionTableProps {
+    automaton: Automaton;
+}
+
+// Definir una estructura que asegure que 'state' no se confunda con los símbolos
+export interface TransitionRow {
+    state: number; // La propiedad state es específica y no se mezcla con los índices dinámicos
+    transitions: {
+        [symbol: string]: number[] | undefined; // El resto son símbolos dinámicos que representan las transiciones
+    };
+}
