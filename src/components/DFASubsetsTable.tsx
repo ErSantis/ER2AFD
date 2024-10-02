487653ef222415df1df183fc60cc7b788f45db73
Elkin <ersantis@uninorte.edu.co>
2024-10-01 22:39:40 -0500

    feat:DFASubsetsTable component to display the correspondence between AFN sets and DFA states

---
+++ src/components/DFASubsetsTable.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { State } from '../models/State';
+
+interface AFNToDFAStateMapProps {
+  conjuntoAFNMap: Map<string, Set<State>>;
+}
+
+const AFNToDFAStateMap: React.FC<AFNToDFAStateMapProps> = ({ conjuntoAFNMap }) => {
+  return (
+    <div>
+      <h2>Correspondencia de Conjuntos AFN a Estados DFA</h2>
+      <table className="transition-table">
+        <thead>
+          <tr>
+            <th>Estado DFA</th>
+            <th>Conjunto de Estados AFN</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.from(conjuntoAFNMap.entries()).map(([letra, conjunto]) => (
+            <tr key={letra}>
+              <td>{letra}</td>
+              <td>
+                {'{'}
+                {Array.from(conjunto)
+                  .map((state) => state.id)
+                  .sort((a, b) => a - b)
+                  .join(', ')}
+                {'}'}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default AFNToDFAStateMap;
