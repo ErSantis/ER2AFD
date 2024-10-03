300a4a693939c3b5743f38d7783ad21eb28d6447
Elkin <ersantis@uninorte.edu.co>
2024-10-03 15:12:44 -0500

    Refactor DFATab.tsx to include isMinimized prop in DFATabProps

--- src/types/DFATab.type.ts
+++ src/types/DFATab.type.ts
@@ -1,11 +1,10 @@
 import { State } from "../models/State";
 
 export interface DFATabProps {
-    dfaTransitions: Map<string, Map<string, string>>;
-    symbols: string[];
-    estadosFinales: Set<string>;
-    estadoInicial: string;
-    conjuntoAFNMap:  Map<string, Set<State>>
-    estadoSignificativos?:  Map<string, Set<State>>
-  }
-  
+  dfaTransitions: Map<string, Map<string, string>>;
+  symbols: string[];
+  estadosFinales: Set<string>;
+  estadoInicial: string;
+  conjuntoAFNMap: Map<string, Set<State>>;
+  isMinimized: boolean;  // Nueva prop
+}
