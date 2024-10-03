b7200fa3149d27e8aea83e78d98a8a53e6357fa9
Elkin <ersantis@uninorte.edu.co>
2024-10-03 15:12:30 -0500

    Refactor DFATab.tsx to include functionality for updating the DOT representation dynamically

--- src/components/DFATab.tsx
+++ src/components/DFATab.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AutomatonGraph from './AutomatonGraph';
 import { dfaToDot } from '../utils/AutomatonToDot';
 import DFATransitionTable from './DFATransitionTable';
 import AFNToDFAStateMap from './DFASubsetsTable';
 import { DFATabProps } from '../types/DFATab.type';
 
-const DFATab: React.FC<DFATabProps> = ({ dfaTransitions, symbols, estadosFinales, estadoInicial, conjuntoAFNMap, estadoSignificativos }) => {
-  //Ver en estados significativos que letra tiene el mismo conjunto de estados y eliminar la segunda aparicion
-  
-  
+const DFATab: React.FC<DFATabProps> = ({
+  dfaTransitions,
+  symbols,
+  estadosFinales,
+  estadoInicial,
+  conjuntoAFNMap,
+  isMinimized,
+}) => {
+  const [dot, setDot] = useState<string>(''); // Estado para el string DOT
+
+  // Regenerar la cadena DOT cada vez que cambien las transiciones, símbolos o estados
+  useEffect(() => {
+    if (dfaTransitions && symbols.length > 0) {
+      const dotRepresentation = dfaToDot(Array.from(dfaTransitions.entries()), symbols, estadosFinales, estadoInicial);
+      setDot(dotRepresentation); // Actualizamos el estado de DOT
+    }
+  }, [dfaTransitions, symbols, estadosFinales, estadoInicial]); // Aseguramos que estas dependencias están observadas
+
   return (
     <div>
+      <h2>{isMinimized ? 'Automata Finito Determinista Minimizado (mDFA)' : 'Automata Finito Determinista (DFA)'}</h2>
+
       {/* Renderizar el gráfico del AFD */}
-      <h2>Automata Finito Determinista (DFA)</h2>
-      <AutomatonGraph dot={dfaToDot(Array.from(dfaTransitions.entries()), symbols, estadosFinales, estadoInicial)} />
+      <AutomatonGraph dot={dot} />
 
       {/* Renderizar la tabla de transiciones utilizando el nuevo componente */}
-      <DFATransitionTable dfaTransitions={dfaTransitions} symbols={symbols} estadosFinales={estadosFinales} estadoInicial={estadoInicial}/>
+      <DFATransitionTable
+        dfaTransitions={dfaTransitions}
+        symbols={symbols}
+        estadosFinales={estadosFinales}
+        estadoInicial={estadoInicial}
+      />
 
       {/* Renderiza la tabla de estados del AFD y su equivalencia en estados del AFN */}
+      <h2>{isMinimized ? "Estados significativos" : "Correspondencia de Conjuntos AFN a Estados DFA"}</h2>
       <AFNToDFAStateMap conjuntoAFNMap={conjuntoAFNMap} />
     </div>
   );
