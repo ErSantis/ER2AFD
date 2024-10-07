f1bf482ec2e5f7b268907a3308ef6f6cf4595631
Elkin <ersantis@uninorte.edu.co>
2024-10-06 23:15:13 -0500

    Refactor input validation logic in validateRegex.ts

---
+++ src/utils/validateRegex.ts
@@ -0,0 +1,73 @@
+export const validateRegex = (input: string, setIsButtonEnabled: (enabled: boolean) => void) => {
+    // Si el input está vacío, deshabilitamos el botón
+    if (input.trim() === '') {
+        setIsButtonEnabled(false);
+        return;
+    }
+
+    // Validar que empiece con algo válido (un carácter o una expresión entre paréntesis)
+    const validStartRegex = /^([^)\\|*?+]+.*)$/;
+    if (!validStartRegex.test(input)) {
+        setIsButtonEnabled(false);
+        return;
+    }
+
+    // Validar que no contenga caracteres reservados
+    // const hasReservedCharacters = /[\\\"]/;
+    // if (hasReservedCharacters.test(input)) {
+    //     setIsButtonEnabled(false);
+    //     return;
+    // }
+
+    // Validar que no se repitan más de una vez los caracteres ?, +, y *
+    const invalidRepeatRegex = /(\?|\+|\*)(\?|\+|\*)+/;
+    if (invalidRepeatRegex.test(input)) {
+        setIsButtonEnabled(false);
+        return;
+    }
+
+    // Validar que los | tengan algo a ambos lados
+    const arePipesValid = (input: string): boolean => {
+        // Asegurarse de que no haya pipes consecutivos sin nada válido entre ellos
+        const invalidPipesRegex = /\|\||\(\||\|\)|\|[*+?)]|(?<!\()\|(?=\))/;
+
+        // Retorna true si no hay casos de pipes inválidos
+        return !invalidPipesRegex.test(input);
+    };
+
+    // Si el input contiene un pipe y no pasa la validación de pipes, desactiva el botón
+    if (input.includes("|") && !arePipesValid(input)) {
+        setIsButtonEnabled(false);
+        return;
+    }
+
+    // Validar que los parentesis no esten vacios
+    const hasEmptyParentheses = (input: string): boolean => {
+        const emptyParenthesesRegex = /\(\)|\([*+?|]\)/;
+        return emptyParenthesesRegex.test(input);
+    };
+    if (hasEmptyParentheses(input)) {
+        setIsButtonEnabled(false);
+        return;
+    }
+    const areParenthesesBalanced = (str: string): boolean => {
+        let stack: string[] = [];
+        for (let char of str) {
+            if (char === '(') {
+                stack.push(char);
+            } else if (char === ')') {
+                if (stack.length === 0) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.length === 0;
+    };
+
+    if (!areParenthesesBalanced(input)) {
+        setIsButtonEnabled(false);
+        return;
+    }
+    setIsButtonEnabled(true);
+};
